ca2bb7fa31ddf50d7aba35dff4c9f242cdb90a3f
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-07-21 12:11:59 +0300

    Configurating environments

---
+++ HobbyWorld/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'hobbyworld-93522',
+    appId: '1:628934118332:web:bff0e1d804ae0a588339b9',
+    databaseURL: 'https://hobbyworld-93522-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'hobbyworld-93522.appspot.com',
+    apiKey: 'AIzaSyBdNCK3J5OPrmt6NdW9HNSAWLWaiK7pAvs',
+    authDomain: 'hobbyworld-93522.firebaseapp.com',
+    messagingSenderId: '628934118332',
+    measurementId: 'G-09T2XMDJ1H',
+  },};
---
+++ HobbyWorld/src/environments/environment.ts
@@ -0,0 +1,17 @@
+
+
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyBdNCK3J5OPrmt6NdW9HNSAWLWaiK7pAvs",
+        authDomain: "hobbyworld-93522.firebaseapp.com",
+        databaseURL: "https://hobbyworld-93522-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "hobbyworld-93522",
+        storageBucket: "hobbyworld-93522.appspot.com",
+        messagingSenderId: "628934118332",
+        appId: "1:628934118332:web:bff0e1d804ae0a588339b9",
+        measurementId: "G-09T2XMDJ1H"
+    },
+
+};
+
+
