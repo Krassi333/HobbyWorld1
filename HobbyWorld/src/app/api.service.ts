c4073f6de95a844ae95892ac80a35f5417d68911
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-07-25 08:51:24 +0300

    Create api service

---
+++ HobbyWorld/src/app/api.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+import { IPost } from './types/post';
+import { Database, onValue, ref } from '@angular/fire/database'; 
+
+
+//const {apiUrl}=environment.firebase.databaseURL;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient,private database:Database) {  }
+
+  getAllPosts() {
+  
+    return this.http.get<IPost[]>(`https://hobbyworld-93522-default-rtdb.europe-west1.firebasedatabase.app/Posts`);
+  }
+
+  getPostById(id:string){
+    return this.http.get<IPost>(`https://hobbyworld-93522-default-rtdb.europe-west1.firebasedatabase.app/Posts/${id}`);
+  }
+   
+ 
+}
