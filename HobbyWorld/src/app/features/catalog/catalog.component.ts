ca35b973af5378da8817e76672f55ab0de275598
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-07-26 22:23:44 +0300

    Create catalog component

---
+++ HobbyWorld/src/app/features/catalog/catalog.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { ApiService } from 'src/app/api.service';
+import { IPost } from 'src/app/types/post';
+
+@Component({
+  selector: 'app-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent implements OnInit {
+
+  @Input() post!: IPost;
+
+  postsList: [string, IPost][]=[];
+  isLoading: boolean = true;
+
+  constructor(private apiService: ApiService) {
+
+  }
+  ngOnInit(): void {
+    this.apiService.getAllPosts()
+      .subscribe({
+        next: (posts) => {
+          console.log(Object.entries(posts));
+          
+          this.postsList = Object.entries(posts);
+          console.log(this.postsList);
+          this.isLoading = false;
+         
+        },
+        error: (err) => {
+          this.isLoading = false;
+          console.log('Error: ' + err);
+        }
+      }
+      )
+
+    
+  }
+
+
+}
+
